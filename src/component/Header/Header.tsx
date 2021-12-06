87cfc5cfd3d4757a7c614c5fe0bc270163e49569
dooyeong20 <noviced22@gmail.com>
2021-12-06 13:08:49 +0900

    :balloon: class creator 적용

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import { cls } from '../../util';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
-    <header className={styles.container}>
-      <a href="#" className={styles.logo}>
-        <span className={styles.highlight}>INDIE</span> Playground
+    <header className={cls(styles.container)}>
+      <a href="#" className={cls(styles.logo)}>
+        <span className={cls(styles.highlight)}>INDIE</span> Playground
       </a>
-      <nav className={styles.navList}>
-        <a href="#" className={styles.active}>
+      <nav className={cls(styles.navList)}>
+        <a href="#" className={cls(styles.active)}>
           Home
         </a>
         <a href="#">Post</a>
         <a href="#">Review</a>
-        <a href="#" className={styles.sign}>
+        <a href="#" className={cls(styles.sign)}>
           Sign In
         </a>
-        <a href="#" className={styles.sign}>
+        <a href="#" className={cls(styles.sign)}>
           Sign Up
         </a>
       </nav>
-      <div role="button" className={styles.navButton}>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
+      <div role="button" className={cls(styles.navButton)}>
+        <div className={cls(styles.line)}></div>
+        <div className={cls(styles.line)}></div>
+        <div className={cls(styles.line)}></div>
       </div>
     </header>
   );
