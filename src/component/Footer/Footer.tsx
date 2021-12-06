9c0b442b55d6a922d1c8af917dd553ae988a557c
dooyeong20 <noviced22@gmail.com>
2021-12-06 13:50:20 +0900

    :balloon: Footer 컴포넌트 작성

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import { cls } from '../../util';
 
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={cls(styles.container)}>
+      <span className={cls(styles.paragraph)}>
+        © 2021 INDIE Playground. All rights reserved.
+      </span>
+      <span className={cls(styles.paragraph)}>
+        개발자 이메일: noviced22@gmail.com{' '}
+      </span>
+    </div>
+  );
 }
