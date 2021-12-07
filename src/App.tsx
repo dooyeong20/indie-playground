c3a34154cc94f8c8eb75e6a4963b6c09c60e9ae1
dooyeong20 <noviced22@gmail.com>
2021-12-08 00:31:23 +0900

    :balloon: redux view mode 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import * as _ from 'lodash';
 import { HomePage } from './view/HomePage';
+import { useDispatch } from 'react-redux';
+import { changeToDesktopView, changeToMobileView } from './store/appSlice';
 
 export function App() {
+  const dispatch = useDispatch();
+  useLayoutEffect(() => {
+    dispatch(
+      window.innerWidth < 800 ? changeToMobileView() : changeToDesktopView()
+    );
+    const handleResize = _.throttle(() => {
+      const innerWidth = window.innerWidth;
+      dispatch(innerWidth < 800 ? changeToMobileView() : changeToDesktopView());
+    }, 100);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [dispatch]);
+
   return <HomePage />;
 }
