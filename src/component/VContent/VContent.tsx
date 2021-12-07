acb45ea601e273263a89e134250c53bd995def88
dooyeong20 <noviced22@gmail.com>
2021-12-08 00:30:49 +0900

    :balloon: Post 구현 완료
    - css 수정 필요

--- src/component/VContent/VContent.tsx
+++ src/component/VContent/VContent.tsx
@@ -5,11 +5,13 @@ import styles from './VContent.module.css';
 export function VContent({ imgPath }: { imgPath: string }) {
   return (
     <div className={cls(styles.container)}>
-      <img
-        src={imgPath}
-        alt="임시 이미지 alt"
-        className={cls(styles.imageBox)}
-      />
+      <picture className={cls(styles.pictureBox)}>
+        <img
+          src={imgPath}
+          alt="임시 이미지 alt"
+          className={cls(styles.imageBox)}
+        />
+      </picture>
       <p className={cls(styles.detail)}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi,
         veniam.
