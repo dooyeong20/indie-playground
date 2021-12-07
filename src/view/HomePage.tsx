3ec48763b2b5cc5590309dafd66cae073f3f6f27
dooyeong20 <noviced22@gmail.com>
2021-12-08 00:31:09 +0900

    :refactor: 홈페이지 view 수정

--- src/view/HomePage.tsx
+++ src/view/HomePage.tsx
@@ -9,18 +9,11 @@ import {
 
 export function HomePage() {
   return (
-    <div>
+    <>
       <Header />
       <main>
         <HotContent />
-        <HorizontalContentBox
-          title="Post"
-          items={[
-            'https://play-lh.googleusercontent.com/2_M3GXptvR4z83AuWce9xAKKKhCOEZLVOeediIMRSe5hcKApDmLVefT9q8xpZZrWTB0=s96-rw',
-            'https://play-lh.googleusercontent.com/DJexP6PO8dL06XvNrjG7plb7SW_SaxuNamO80ab512JA71lBEBUnaJCaZzlqWVrrlEiG=s128-rw',
-            'https://play-lh.googleusercontent.com/DzZ3En2Wuhx8Jw3FsVsSsyfzT2csAOC5n4H8a64mXmiVp-N_lweEMSY62lbX8Ksc7Ps=s128-rw',
-          ]}
-        />
+        <HorizontalContentBox title="Post" />
         <VerticalContentBox
           title="Review"
           items={[
@@ -33,6 +26,6 @@ export function HomePage() {
         />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
