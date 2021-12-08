bd75d5a6f66c0955c90f589b418184c83493d214
dooyeong20 <noviced22@gmail.com>
2021-12-08 11:17:52 +0900

    :balloon: 타입 정의

--- src/@types/store.ts
+++ src/@types/store.ts
@@ -4,6 +4,16 @@ export enum EViewMode {
   mobile = 'mobile',
 }
 
+export enum EUserStatus {
+  guest = 'guest',
+  member = 'member',
+}
+
+export type TUserState = {
+  status: EUserStatus;
+  userName: string;
+};
+
 export type TAppState = {
   viewMode: EViewMode;
 };
