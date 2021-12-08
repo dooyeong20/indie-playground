8bbf29bb0be6744141d35a387225e70875a95de2
dooyeong20 <noviced22@gmail.com>
2021-12-08 12:32:54 +0900

    :balloon: 모바일 헤더 네비게이션 토글 구현

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { EUserStatus, EViewMode } from '../../@types';
+import { TRootState } from '../../store';
+import { loginUser, logout } from '../../store/userSlice';
 import { cls } from '../../util';
 import styles from './Header.module.css';
+import { MobileNavigation } from '..';
 
 export function Header() {
+  const [isNavOpen, setisNavOpen] = useState(false);
+  const user = useSelector((state: TRootState) => state.user);
+  const viewMode = useSelector((state: TRootState) => state.app.viewMode);
+  const dispatch = useDispatch();
+  const handleClickSignIn = () => {
+    dispatch(loginUser({ status: EUserStatus.member, userName: 'test user' }));
+  };
+  const handleClickSignOut = () => {
+    dispatch(logout());
+  };
+  const handleClickOpenNav = () => {
+    setisNavOpen(true);
+  };
+  const handleClickCloseNav = () => {
+    setisNavOpen(false);
+  };
+  const isGuest = () => user.status === EUserStatus.guest;
+
   return (
     <header className={cls(styles.container)}>
       <a href="#" className={cls(styles.logo)}>
@@ -14,18 +37,43 @@ export function Header() {
         </a>
         <a href="#">Post</a>
         <a href="#">Review</a>
-        <a href="#" className={cls(styles.sign)}>
+        <a
+          href="#"
+          className={cls(isGuest() ? styles.sign : styles.hidden)}
+          onClick={handleClickSignIn}
+        >
           Sign In
         </a>
-        <a href="#" className={cls(styles.sign)}>
+        <a href="#" className={cls(isGuest() ? styles.sign : styles.hidden)}>
           Sign Up
         </a>
+        <a href="#" className={cls(isGuest() ? styles.hidden : styles.sign)}>
+          My Page
+        </a>
+        <a
+          href="#"
+          className={cls(isGuest() ? styles.hidden : styles.sign)}
+          onClick={handleClickSignOut}
+        >
+          Sign Out
+        </a>
       </nav>
-      <div role="button" className={cls(styles.navButton)}>
+      <div
+        role="button"
+        className={cls(styles.navButton)}
+        onClick={handleClickOpenNav}
+      >
         <div className={cls(styles.line)}></div>
         <div className={cls(styles.line)}></div>
         <div className={cls(styles.line)}></div>
       </div>
+      <MobileNavigation
+        isOpen={viewMode !== EViewMode.desktop && isNavOpen}
+        isGuest={isGuest()}
+        onClickCloseNav={handleClickCloseNav}
+        onClickSignIn={handleClickSignIn}
+        onClickSignOut={handleClickSignOut}
+      />
     </header>
   );
 }
