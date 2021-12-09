d685bd9dc0e6c880c0f2bba4d63c6822f21ab423
dooyeong20 <noviced22@gmail.com>
2021-12-09 16:48:33 +0900

    :balloon: 컨텐츠 커버 페이지 헤더 컴포넌트 작성

---
+++ src/component/ContentsCoverHeader/ContentsCoverHeader.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { cls } from '../../util';
+import styles from './ContentsCoverHeader.module.css';
+import searchSVG from '../../assets/search.svg';
+
+export function ContentsCoverHeader() {
+  return (
+    <div className={cls(styles.container)}>
+      <div>
+        <h2 className={cls(styles.category)}>Category</h2>
+        <div className={cls(styles.hScroll)}>
+          <ul className={cls(styles.categoryList)}>
+            <li className={cls(styles.categoryItem)}>롤플레잉</li>
+            <li className={cls(styles.categoryItem)}>시뮬레이션</li>
+            <li className={cls(styles.categoryItem)}>액션</li>
+            <li className={cls(styles.categoryItem)}>+</li>
+          </ul>
+        </div>
+      </div>
+      <form action="#" className={cls(styles.form)}>
+        <label htmlFor="search" className={cls(styles.label)}>
+          검색
+        </label>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search Games!"
+          className={cls(styles.input)}
+          style={{
+            background: `center left 0 url(${searchSVG}) no-repeat`,
+          }}
+        />
+      </form>
+    </div>
+  );
+}
--- src/component/index.ts
+++ src/component/index.ts
@@ -4,6 +4,7 @@ export * from './Footer/Footer';
 export * from './HorizontalContentBox/HorizontalContentBox';
 export * from './VerticalContentBox/VerticalContentBox';
 export * from './MobileNavigation/MobileNavigation';
+export * from './ContentsCoverHeader/ContentsCoverHeader';
 export * from './VContent/VContent';
 export * from './HContent/HContent';
 export * from './Loading/Loading';
