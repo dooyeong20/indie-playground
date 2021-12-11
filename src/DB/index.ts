de099c3e8eb012bfbfac233cda72274f0d2178fe
dooyeong20 <noviced22@gmail.com>
2021-12-12 00:32:35 +0900

    :balloon: 데이터 전송 실수 방지용 로그

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -2,11 +2,13 @@ import { collection, getDocs, limit, query } from 'firebase/firestore';
 import { db } from '../firebase-config';
 
 export const getReviews = async (count: number) => {
+  console.log('getting reviews ...');
   const q = query(collection(db, 'review'), limit(count));
   return getDocs(q);
 };
 
 export const getPosts = async (count: number) => {
+  console.log('getting posts ...');
   const q = query(collection(db, 'post'), limit(count));
   return getDocs(q);
 };
