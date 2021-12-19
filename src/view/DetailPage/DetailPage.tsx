658546c9b74452811815c9fc4f0379cd098a604f
dooyeong20 <noviced22@gmail.com>
2021-12-19 19:08:58 +0900

    :balloon: Detail 페이지 content 설정
    - firebase 연동

--- src/view/DetailPage/DetailPage.tsx
+++ src/view/DetailPage/DetailPage.tsx
@@ -1,41 +1,43 @@
-import React from 'react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { EContentType } from '../../@types';
+import { TContent } from '../../@types';
 import {
   CarouselContainer,
   CommentContainer,
   DetailTextContent,
 } from '../../component';
+import { db } from '../../firebase-config';
 
 type TDetailPageParam = 'id' | 'category';
 
 export function DetailPage() {
+  const [content, setContent] = useState<TContent | null>(null);
   const { id, category } = useParams<TDetailPageParam>();
 
-  console.log(id, category);
+  const getDetail = async (category: string, id: string) => {
+    console.log('getting details ...');
+    const q = query(collection(db, category), where('id', '==', id));
+    const snapshot = await getDocs(q);
+
+    snapshot.forEach((doc) => {
+      setContent(doc.data() as TContent);
+    });
+  };
+
+  useEffect(() => {
+    getDetail(category!, id!);
+  }, [id, category]);
+
+  if (!content) {
+    return null;
+  }
 
   return (
     <>
-      <CarouselContainer
-        contents={[
-          {
-            id: '11',
-            title: 'tmp title',
-            type: EContentType.post,
-            mainImagePath:
-              'https://i.ytimg.com/vi/HtOGr6vr9eU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDZ2b8vMOexjPBY9ypp-iF9HOf69Q',
-          },
-          {
-            id: '12',
-            title: 'tmp title',
-            type: EContentType.post,
-            mainImagePath:
-              'https://i.ytimg.com/vi/cxfyPzjNxLA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDFMUjZYSaUqHNZusqFUWhPSegPFg',
-          },
-        ]}
-      />
+      <CarouselContainer contents={[content]} />
       <DetailTextContent
-        title="임시 타이틀"
+        title={content.title}
         text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo eveniet hic culpa, deleniti rem maiores, vero ullam, beatae minus repellat odio ab accusantium quas deserunt dolore temporibus minima voluptate modi."
       />
       <CommentContainer />
