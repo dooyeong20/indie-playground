27703c6a17a63ee5db0cd50ed6216ed116b70e5e
dooyeong20 <noviced22@gmail.com>
2021-12-29 14:42:24 +0900

    :balloon: 로그인시 유저 정보 저장

--- src/view/SigninPage/SigninPage.tsx
+++ src/view/SigninPage/SigninPage.tsx
@@ -7,27 +7,70 @@ import { loginUser } from '../../store/userSlice';
 import { EUserStatus } from '../../@types';
 import { AuthButton } from '../../component';
 import { ReactComponent as GoogleLogo } from '../../assets/googleLogo.svg';
+import { useNavigate } from 'react-router-dom';
+import { signinUser } from '../../DB';
 
 export default function SigninPage() {
   const auth = getAuth();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const provider = useMemo(() => new GoogleAuthProvider(), []);
   const handleGoogleSignin = async () => {
-    const res = await signInWithPopup(auth, provider);
-    const { displayName, email } = res.user;
-    dispatch(
-      loginUser({
-        status: EUserStatus.member,
-        displayName: displayName ?? 'no name',
-        email: email ?? 'no email',
-      })
-    );
+    try {
+      const res = await signInWithPopup(auth, provider);
+      const { displayName, email } = res.user;
+      if (!displayName) {
+        throw new Error('no user name found');
+      }
+      if (!email) {
+        throw new Error('no email found');
+      }
+      dispatch(
+        loginUser({
+          status: EUserStatus.member,
+          displayName: displayName,
+          email: email ?? 'no email',
+        })
+      );
+      signinUser(displayName, email);
+      navigate('/');
+    } catch (e) {
+      console.error(e);
+    }
   };
+  // TODO: kakao 401 에러 해결 필요
+  // const handleKakaoSignin = async () => {
+  //   location.href =
+  //     'https://kauth.kakao.com/oauth/authorize?client_id=1c7898eda83bd793b9d070dc1c5621cb&redirect_uri=http://localhost:3000/signin&response_type=code';
+  // };
+  // const getToken = async (code: string) => {
+  //   if (!code) {
+  //     return;
+  //   }
+
+  //   const bodyData = {
+  //     grant_type: 'authorization_code',
+  //     client_id: '1c7898eda83bd793b9d070dc1c5621cb',
+  //     redirect_uri: 'http://localhost:3000/signin',
+  //     code,
+  //     client_secret: 'GPQSv4puVFD0rmepmi54fiADm7QehLXO',
+  //   };
+
+  //   const res = await axios.post(
+  //     'https://kauth.kakao.com/oauth/token',
+  //     bodyData
+  //   );
+
+  //   console.log(res.headers);
+  // };
 
   useEffect(() => {
     provider.setCustomParameters({
       prompt: 'select_account',
     });
+
+    // const code = search.slice(6);
+    // getToken(code);
   }, [provider]);
 
   return (
@@ -37,8 +80,14 @@ export default function SigninPage() {
         <AuthButton
           LogoSVG={GoogleLogo}
           onClick={handleGoogleSignin}
-          title="Google 계정으로 로그인"
+          title="Google 계정으로 시작하기"
         />
+        {/* TODO: kakao 401 에러 해결 필요 */}
+        {/* <AuthButton
+          LogoSVG={GoogleLogo}
+          onClick={handleKakaoSignin}
+          title="Kakao 계정으로 시작하기"
+        /> */}
       </div>
     </div>
   );
